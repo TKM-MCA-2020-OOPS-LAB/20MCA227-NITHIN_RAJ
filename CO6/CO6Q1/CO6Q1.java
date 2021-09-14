973931d0aec0a9fbb50dd1b9e3bcdb743758cb40
Nithin <nithinraj10@hotmail.com>
2021-09-14 20:58:56 +0530

    CO6Q1 - Auto stash before rebase of "refs/heads/main"

---
+++ CO6/CO6Q1/CO6Q1.java
@@ -0,0 +1,41 @@
+//Program to list the sub directories and files in a given directory and also search for a file
+//name.
+package javaprograms;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class CO6Q1 {
+    public static void main(String[] args) {
+        int flag=0;
+        File f = new File("../");
+        Scanner sc = new Scanner(System.in);
+        String name,filename;
+        String[] filelist = f.list();
+        try{
+        for(String str: filelist)
+            System.out.println(str);
+        System.out.println("**********");
+        System.out.println("Enter the file name to search:");
+        name = sc.nextLine();
+        for(int i =0; i<f.length();i++)
+        {
+            filename = filelist[i];
+            if(filename.equals(name))
+            {
+                flag=1;
+                break;
+            }
+            else
+                flag=0;
+        }
+        }
+        catch(Exception e)
+        {
+        if(flag==1)
+            System.out.println("File found");
+        else
+            System.out.println("File not found");
+        }
+    }
+}
